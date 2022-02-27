4c7973628b76e7a00c5d50626732b6439521e17e
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2022-02-27 12:37:35 +0700

    adding example of http response's header

--- src/com/progjar/Main.kt
+++ src/com/progjar/Main.kt
@@ -1,6 +1,9 @@
 package com.progjar
 
 import com.progjar.parser.parseLink
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.net.Socket
 
 const val TEXT_RESET = "\u001B[0m"
 const val TEXT_BLACK = "\u001B[30m"
@@ -18,5 +21,40 @@ fun main(args: Array<String>) {
 //    println("Google(https://www.google.com)")
 //    println(TEXT_RED + "This text is red!" + TEXT_RESET)
 
-    parseLink( "<a href=\"www.google.com\">google</a>")
+//    parseLink( "<a href=\"www.google.com\">google</a>")
+    connectToLink( "monta.if.its.ac.id" )
+}
+
+fun connectToLink( link: String ) {
+    try {
+        // 1. create a new socket object
+        var socket:Socket = Socket(link, 80)
+
+        // 2. obtain the input and output
+        var bis = BufferedInputStream( socket.getInputStream() )
+        var bos = BufferedOutputStream( socket.getOutputStream() )
+
+        // 3. exchange messages
+        bos.write( "hello world\r\n".toByteArray() )
+        bos.flush()
+
+        //
+        var bResp = bis.readAllBytes()
+//        var bResp = bis.readNBytes( 100 )
+        var resp = String( bResp )
+        print( resp )
+
+//        var resp = ""
+//        while( bResp.size < 100 ) {
+//            resp += bResp.toString()
+//            bResp = bis.readNBytes( 100 )
+//        }
+
+//        print( resp )
+
+        // 4. close connection
+        socket.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
